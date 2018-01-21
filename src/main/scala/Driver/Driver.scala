bf270bd5543df73e7096b5b62e68b6cb2079f5a8
victor-obrien-iv <victor.obrien.iv@gmail.com>
2018-01-21 16:03:09 -0500

    Adds tokenizer framework

--- src/main/scala/Driver/Driver.scala
+++ src/main/scala/Driver/Driver.scala
@@ -1,9 +1,25 @@
 package Driver
 
+import scala.io.{BufferedSource, Source}
+
 object Driver {
   def main(args: Array[String]): Unit = {
     try {
       CommandLine parseArgs args
+
+      if ( CommandLine.files.isEmpty ) throw Error.Error( "", "No .java file specified", Error.Type.CommandLine )
+
+      // TODO: do we even need worry about multiple files?
+      for( f <- CommandLine.files ) {
+        val file: BufferedSource = Source.fromFile( f )
+
+        val tokens = Lexer.Lexer.tokenize( file )
+
+        for( t <- tokens ) println( t ) // just print out the tokens for now
+        file.close
+
+
+      }
 
 
     } catch {
