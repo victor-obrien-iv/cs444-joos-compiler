84cd996f7bb20c8e716fe46aec1dac95a8bf90f5
Victor Joseph OBrien <vjobrien@uwaterloo.ca>
2018-02-01 18:29:21 -0500

    Refactors state object into status class

--- src/main/scala/Lexer/Lexer.scala
+++ src/main/scala/Lexer/Lexer.scala
@@ -5,6 +5,7 @@ import akka.pattern.ask
 import akka.actor.{ActorRef, ActorSystem, Props}
 import akka.util.Timeout
 
+import scala.collection.mutable
 import scala.collection.mutable.{ListBuffer, Map, PriorityQueue}
 import scala.concurrent.duration._
 import scala.concurrent.{Await, Future}
@@ -12,92 +13,74 @@ import scala.language.postfixOps
 
 object Lexer {
   val actorSystem = ActorSystem( "actorSystem" )
-  val DFAs: Array[ActorRef] = Array(
-    actorSystem.actorOf( Props[LiteralDFA], "LiteralDFA" )
-  )
 
-  object state {
+  case class Status(fileList: List[Char]) {
+    private val file: List[Char] = fileList
     private var lexeme: String = ""
-    private var row, col: Int = 0
-    private val eofChar = '\u0000'
-    private var char: Char = eofChar
+    private var row: Int = 1
+    private var col: Int = 1
+    private var charNum: Int = 0
+    private var prev: Int = 0
+    private def char: Char = { file.apply(charNum) }
+
     def getLexeme: String = { lexeme }
     def getRow: Int = { row }
     def getCol: Int = { col }
     def getChar: Char = { char }
 
-    private var charNum: Int = -1
-    private var prev: Int = 0
-    private[Lexer] def advance(file: List[Char]): Boolean = {
+    private[Lexer] def advance(): Unit = {
       charNum += 1
       col += 1
-      if ( charNum >= file.size ) {
-        // we've hit eof
-        char = eofChar
-        return false
+      if ( !eof ) {
+        if ( char == '\n' ) {
+          row += 1
+          col = 0
+        }
+        lexeme += char
       }
-
-      char = file.apply(charNum)
-      lexeme += char
-
-      if ( char == '\n' ) {
-        row += 1
-        col = 0
-      }
-
-      true
     }
     private[Lexer] def restoreTo(token: Token.Token): Unit = {
-      row = token.row
+      row = token.row //- 1
       col = token.col
       charNum = prev + token.lexeme.toString.length
       prev = charNum
       lexeme = ""
     }
     private[Lexer] def eof: Boolean = {
-      if (char == eofChar) true
-      else false
+      if ( charNum < file.size ) false
+      else true
     }
-
     // returns false if eof is hit, true otherwise
-    private[Lexer] def trimWhitespace(file: List[Char]): Boolean = {
-      while ( charNum < file.size
-              && DFA.whitespace.contains( file.apply(charNum) )) {
-        if ( file.apply(charNum) == '\n' ) {
-          row += 1
-          col = 0
-        }
-        charNum += 1
-        col += 1
+    private[Lexer] def trimWhitespace(): Boolean = {
+      while ( !eof && DFA.whitespace.contains( file.apply(charNum) )) {
+        advance()
       }
-      if ( charNum < file.size ) {
-        char = file.apply(charNum)
+      if ( !eof ) {
         prev = charNum
         lexeme = char.toString
         true
       } else {
-        char = eofChar
         lexeme = ""
         false
       }
     }
   }
 
-
   def tokenize ( file: BufferedSource ): List[Token.Token] = {
     var tokens: ListBuffer[Token.Token] = ListBuffer()
-    val fileList = file.toList
+    val status = Status(file.toList)
+    val DFAs: Array[ActorRef] = Array(
+      actorSystem.actorOf( Props(new LiteralDFA(status)), "LiteralDFA" )
+    )
 
-    state.advance(fileList)
-
-    while( state.trimWhitespace(fileList) ) {
+    while( status.trimWhitespace() ) {
 
       // Meta data containers
       implicit object Ord extends Ordering[(Int, Token.Token)] {
-        def compare(x: (Int, Token.Token), y: (Int, Token.Token)) = y._1.compare(x._1)
+        def compare(x: (Int, Token.Token), y: (Int, Token.Token)): Int = y._1.compare(x._1)
       }
-      var resultHeap: PriorityQueue[(Int, Token.Token)] = PriorityQueue.empty[(Int, Token.Token)]
-      val futures: Map[ActorRef, Option[Future[Any]]] = Map( DFAs.apply( 0 ) -> None )
+      var resultHeap: mutable.PriorityQueue[(Int, Token.Token)] = mutable.PriorityQueue.empty[(Int, Token.Token)]
+      val futures: mutable.Map[ActorRef, Option[Future[Any]]] = mutable.Map( DFAs.apply( 0 ) -> None )
       var activeDFAs: Set[ActorRef] = DFAs.toSet
 
       do {
@@ -105,8 +88,8 @@ object Lexer {
 
         // ask each dfa
         for( dfa <- activeDFAs ) {
-          if ( !state.eof )
-            futures(dfa) = Some(dfa ask state.getChar)
+          if ( !status.eof )
+            futures(dfa) = Some(dfa ask status.getChar)
           else
             // we've hit eof, have each remaining dfa report its last accept state
             futures( dfa ) = Some(dfa ask EOF())
@@ -135,7 +118,9 @@ object Lexer {
           }
         }
 
-        state.advance(fileList)
+        if ( !status.eof ) status.advance()
+        else assert(activeDFAs.isEmpty, "Each DFA must finish after receiving eof")
+
       } while( activeDFAs.nonEmpty )
 
       // the DFAs have finished, take the longest result
@@ -147,12 +132,11 @@ object Lexer {
         tokens += top._2
 
         // reset charNum back to the next char to be processed
-        state.restoreTo(top._2)
-        state.advance(fileList)
+        status.restoreTo(top._2)
       } else {
         // no dfa returned a token, throw an error
         // TODO: throw the error
-        println("Error: " + state.getChar)
+        println("Error: " + status.getChar)
       }
     }
 
