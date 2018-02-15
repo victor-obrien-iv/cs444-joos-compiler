db3df4ae3c0bddef101efad8aad73850f17ad7b4
victor-obrien-iv <victor.obrien.iv@gmail.com>
2018-02-14 19:07:05 -0500

    Adds ast type class

--- src/main/scala/AST/Type.scala
+++ src/main/scala/AST/Type.scala
@@ -1,5 +1,27 @@
 package AST
 
-class Type {
+/**
+  * Type represents a data type
+  */
+sealed trait Type extends AstNode
 
-}
+/**
+  * ArrayType represents an array data type
+  * ex: int[]
+  * @param arrayOf The type of the elements in the array
+  * @param size The expression that denotes the size of the array, if present
+  */
+case class ArrayType(arrayOf: Type, size: Option[Expr]) extends Type
+
+/**
+  * PrimitiveType represents an implicit primitive data type
+  * ex: int
+  * @param typeToken The token that denotes this primitive
+  */
+case class PrimitiveType(typeToken: Token.Primitive) extends Type
+
+/**
+  * ClassType represents a non-primitive type defined as a class
+  * @param typeID The id that denotes the class
+  */
+case class ClassType(typeID: FullyQualifiedID) extends Type
