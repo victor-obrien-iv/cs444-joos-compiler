cb5392ecb51978c54a4dae39fe8b28d946d21b36
victor-obrien-iv <victor.obrien.iv@gmail.com>
2018-02-14 19:05:21 -0500

    Adds ast base class

---
+++ src/main/scala/AST/AstNode.scala
@@ -0,0 +1,20 @@
+package AST
+
+/**
+  * AstNode represents a node in the abstract syntax tree.
+  * This is the base class for Stmt, Decl and Type, and it
+  * does nothing on its own.
+  */
+trait AstNode
+
+/**
+  * FullyQualifiedID is a list of identifiers that specify what Decl the final id refers to.
+  *   This is used when we can be certain that each qn in q1.q2.q3. ... .qn.id is a qualifier
+  *   and not an object.  If we are uncertain, AccessExpr is used instead
+  * ex: myClass.myStaticField
+  *     myPackage.myClass.myStaticField
+  *     myVar
+  * @param qualifiers A potentially empty list of qualifiers leading up to id
+  * @param id The identifier
+  */
+case class FullyQualifiedID(qualifiers: List[Token.Identifier], id: Token.Identifier)
---
+++ src/main/scala/AST/Expr.scala
@@ -0,0 +1,5 @@
+package AST
+
+class Expr {
+
+}
