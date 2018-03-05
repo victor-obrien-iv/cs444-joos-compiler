f2a12de39ea85d23673731fdcaea380dc34a53b5
victor-obrien-iv <victor.obrien.iv@gmail.com>
2018-03-04 21:14:19 -0500

    Fixes commandline error

--- src/main/scala/HierarchyChecker/ExtendsPass.scala
+++ src/main/scala/HierarchyChecker/ExtendsPass.scala
@@ -42,7 +42,7 @@ class ExtendsPass(localContext: Map[String, TypeDecl], typeContext: Map[String, 
             val isFinal = c.modifiers.exists(_.isInstanceOf[Token.JavaFinal])
             if ( isFinal )
               throw Error.Error(extend.id.lexeme, "A class must not extend a final class",
-                Error.Type.ExtendsPass, Some(Error.Location(extend.id.row, extend.id.col, cd.name.lexeme)))
+                Error.Type.ExtendsPass, Some(Error.Location(extend.id.row, extend.id.col, ast.fileName)))
         }
       case None =>
     }
@@ -60,7 +60,7 @@ class ExtendsPass(localContext: Map[String, TypeDecl], typeContext: Map[String, 
       extendType match {
         case _: InterfaceDecl => // do nothing
         case _: ClassDecl =>
-          throw Error.Error(extend.id.lexeme, "A class must not extend a final class",
+          throw Error.Error(extend.id.lexeme, "An interface must not extend a class",
             Error.Type.ExtendsPass, Some(Error.Location(extend.id.row, extend.id.col, ast.fileName)))
       }
     }
